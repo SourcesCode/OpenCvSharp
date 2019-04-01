1d4abb76443e6b6242676b3b56fb700d6c52420e
JasonChen <ijasonchen91@sina.com>
2019-04-01 14:56:05 +0800

    add SamplesCS

--- OpenCvSharp/Program.cs
+++ OpenCvSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SamplesCS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -13,9 +14,54 @@ namespace OpenCvSharp
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Test();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
         }
+
+        private static void Test()
+        {
+            ISample sample =
+            //new BgSubtractorMOG();
+            //new BinarizerSample();
+            //new BRISKSample();
+            //new CaffeSample();
+            //new ClaheSample();
+            //new ConnectedComponentsSample();
+            //new DFT();
+            //new FaceDetection();
+            //new FASTSample();
+            //new FlannSample(); 
+            //new FREAKSample();
+            //new HistSample();
+            //new HOGSample();
+            //new HoughLinesSample();
+            //new KAZESample2();
+            //new KAZESample();
+            //new MatOperations();
+            //new MatToBitmap();
+            //new MDS();
+            //new MSERSample();
+            //new NormalArrayOperations();
+            //new PhotoMethods();
+            //new MergeSplitSample();
+            //new MorphologySample();
+            //new PixelAccess();
+            //new SeamlessClone();
+            //new SiftSurfSample();
+            //new SolveEquation();
+            //new StarDetectorSample();
+            //new Stitching();
+            //new Subdiv2DSample();
+            //new SuperResolutionSample();
+            //new SVMSample();
+            //new VideoWriterSample();
+            new VideoCaptureSample();
+
+            sample.Run();
+
+        }
+
     }
 }
