9558ee04ba3c3f456dd42990a3e373501b136cc7
JasonChen <ijasonchen91@sina.com>
2019-04-28 16:59:07 +0800

    fix

--- OpenCvSharp/Program.cs
+++ OpenCvSharp/Program.cs
@@ -20,48 +20,5 @@ namespace OpenCvSharp
             Application.Run(new Form1());
         }
 
-        private static void Test()
-        {
-            ISample sample =
-            //new BgSubtractorMOG();
-            //new BinarizerSample();
-            //new BRISKSample();
-            //new CaffeSample();
-            //new ClaheSample();
-            //new ConnectedComponentsSample();
-            //new DFT();
-            //new FaceDetection();
-            //new FASTSample();
-            //new FlannSample(); 
-            //new FREAKSample();
-            //new HistSample();
-            //new HOGSample();
-            //new HoughLinesSample();
-            //new KAZESample2();
-            //new KAZESample();
-            //new MatOperations();
-            //new MatToBitmap();
-            //new MDS();
-            //new MSERSample();
-            //new NormalArrayOperations();
-            //new PhotoMethods();
-            //new MergeSplitSample();
-            //new MorphologySample();
-            //new PixelAccess();
-            //new SeamlessClone();
-            //new SiftSurfSample();
-            //new SolveEquation();
-            //new StarDetectorSample();
-            //new Stitching();
-            //new Subdiv2DSample();
-            //new SuperResolutionSample();
-            //new SVMSample();
-            //new VideoWriterSample();
-            new VideoCaptureSample();
-
-            sample.Run();
-
-        }
-
     }
 }
