7d12dff66c03e1e604155e04e5569b12c414da39
JasonChen <ijasonchen91@sina.com>
2019-06-03 12:04:02 +0800

    add

---
+++ OpenCvSharp/ExtendApi/Dtos/DetectRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCvSharp.ExtendApi.Dtos
+{
+    public class DetectRequest
+    {
+        /// <summary>
+        /// 人脸图片文件路径，需要用 post multipart/form-data 的方式上传。
+        /// </summary>
+        public string ImageFilePath { get; set; }
+    }
+}
